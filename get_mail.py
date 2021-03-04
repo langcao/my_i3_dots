7054c582b754fde2ddc862ac94e97af4e6fc87ef
Lang Cao <cao@ircn.jp>
2021-03-04 17:55:22 +0900

    Notification script of emails updated (display part).

--- get_mail.py
+++ get_mail.py
@@ -6,10 +6,10 @@ import os, sys, time
 import html2text
 from email.header import decode_header, make_header
 
-NAME = ['Lang Cao', 'Xun Li', 'Lang Cao']
-ADDRESS = ['lcao@zzu.edu.cn', 'li_xun@ircn.jp', 'cao@ircn.jp']
-PASSWORD = ['miaomiao1982', '20200608lixun', 'miaomiao1982']
-SERVER = ['mail.v.zzu.edu.cn', 'ircn.sakura.ne.jp', 'ircn.sakura.ne.jp']
+NAME = ['Lang Cao', 'Xun Li', 'Lang Cao', 'Xun Li']
+ADDRESS = ['lcao@zzu.edu.cn', 'li_xun@ircn.jp', 'cao@ircn.jp', 'milkquick@gmail.com']
+PASSWORD = ['miaomiao1982', '20200608lixun', 'miaomiao1982','kkmiudnpygdsyflj']
+SERVER = ['mail.v.zzu.edu.cn', 'ircn.sakura.ne.jp', 'ircn.sakura.ne.jp', 'imap.gmail.com']
 
 if len(sys.argv)>1:
     user = int(sys.argv[1])
@@ -44,7 +44,7 @@ def get_content(msg):
 
 def popup_content(content, note_id, level, maxrow=36):
     if isinstance(content, str):
-        lines = content.replace('"',"'").replace('`',"'").split('\n')
+        lines = content.replace('"',"”").replace('`',"｀").replace("'",'’').split('\n')
     else:
         lines = content.split('\n')
 
@@ -64,7 +64,7 @@ def popup_content(content, note_id, level, maxrow=36):
             # print(len(line), pos, line[0:pos])
             if pos>0:
                 if current == 0:
-                    text += ' ' + line[0:pos]+'\n'
+                    text += ' ' + line[0:pos]+'\n'
                 else:
                     text += '  ' + line[0:pos]+'\n'
                 current += 1
@@ -74,50 +74,53 @@ def popup_content(content, note_id, level, maxrow=36):
     os.system('notify-send.sh -r %d -u %s "%s" "%s"'%(note_id, level, text, omitted))
     # print(content)
 
+if user < len(NAME):
 
-imapobj = imaplib.IMAP4_SSL(SERVER[user], '993')
-imapobj.login(ADDRESS[user], PASSWORD[user])
+    imapobj = imaplib.IMAP4_SSL(SERVER[user], '993')
+    imapobj.login(ADDRESS[user], PASSWORD[user])
 
-#workラベル下のメールを取得
-imapobj.select()
-typ, data = imapobj.search(None, 'All')
+    #workラベル下のメールを取得
+    imapobj.select()
+    typ, data = imapobj.search(None, 'All')
 
-#取得したメールから表題と本文を出力し、添付ファイルを同階層に書き出す
-select = data[0].split()
-SHOW_NUM = 1
-for note_id in range(SHOW_NUM):
-    num = select[-note_id-1]
-    typ, data = imapobj.fetch(num, '(RFC822)')
-    mail = email.message_from_string(data[0][1].decode('utf-8'))
-    subject = str(make_header(decode_header(mail["Subject"])))
-    output = " %s"%(subject)
+    #取得したメールから表題と本文を出力し、添付ファイルを同階層に書き出す
+    select = data[0].split()
+    SHOW_NUM = 1
+    for note_id in range(SHOW_NUM):
+        num = select[-note_id-1]
+        typ, data = imapobj.fetch(num, '(RFC822)')
+        mail = email.message_from_string(data[0][1].decode('utf-8'))
+        subject = str(make_header(decode_header(mail["Subject"])))
+        output = " %s"%(subject)
 
-    date = str(make_header(decode_header(mail["Date"])))
-    pos = date.find('+')
-    day = date[0: pos - 10]
-    hms = date[pos - 9: pos - 1]
+        date = str(make_header(decode_header(mail["Date"])))
+        pos = date.find('+')
+        if pos < 0:
+            pos = date.find('-')
+        day = date[0: pos - 10]
+        hms = date[pos - 9: pos - 1]
 
-    sender = str(make_header(decode_header(mail["From"])))
-    output += "\n %s  %s <%s>\n %s   %s"%(sender, NAME[user], ADDRESS[user], day, hms)
+        sender = str(make_header(decode_header(mail["From"])))
+        output += "\n %s  %s <%s>\n %s   %s"%(sender, NAME[user], ADDRESS[user], day, hms)
 
-    count, attachments = 0, ""
-    for part in mail.walk():
-        if part.get_content_maintype() == 'multipart':
-            continue
-        filename = part.get_filename()
-        if filename:
-            count += 1
-            fname, charset = decode_header(filename)[0]
-            if charset:
-                attachments += ' %s\n'%fname.decode(charset)
-    if count>0:
-        output += '   %d Attachment(s)'%count
+        count, attachments = 0, ""
+        for part in mail.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            filename = part.get_filename()
+            if filename:
+                count += 1
+                fname, charset = decode_header(filename)[0]
+                if charset:
+                    attachments += ' %s\n'%fname.decode(charset)
+        if count>0:
+            output += '   %d Attachment(s)'%count
 
-    os.system('notify-send.sh -r %d -u critical "%s" "%s"'%(HEAD_ID, output, attachments))
-    if len(sys.argv)>2:
-        content = get_content(mail)
-        popup_content(content, TEXT_ID, sys.argv[2])
+        os.system('notify-send.sh -r %d -u critical "%s" "%s"'%(HEAD_ID, output, attachments))
+        if len(sys.argv)>2:
+            content = get_content(mail)
+            popup_content(content, TEXT_ID, sys.argv[2])
 
-    time.sleep(4)
+        time.sleep(4)
 
-imapobj.close()
+    imapobj.close()
--- save_mail.py
+++ save_mail.py
@@ -5,10 +5,10 @@ import quopri
 import os, sys, time
 from email.header import decode_header, make_header
 
-NAME = ['Lang Cao', 'Xun Li', 'Lang Cao']
-ADDRESS = ['lcao@zzu.edu.cn', 'li_xun@ircn.jp', 'cao@ircn.jp']
-PASSWORD = ['miaomiao1982', '20200608lixun', 'miaomiao1982']
-SERVER = ['mail.v.zzu.edu.cn', 'ircn.sakura.ne.jp', 'ircn.sakura.ne.jp']
+NAME = ['Lang Cao', 'Xun Li', 'Lang Cao', 'Xun Li']
+ADDRESS = ['lcao@zzu.edu.cn', 'li_xun@ircn.jp', 'cao@ircn.jp', 'milkquick@gmail.com']
+PASSWORD = ['miaomiao1982', '20200608lixun', 'miaomiao1982','kkmiudnpygdsyflj']
+SERVER = ['mail.v.zzu.edu.cn', 'ircn.sakura.ne.jp', 'ircn.sakura.ne.jp', 'imap.gmail.com']
 
 EMAIL_PATH = '/tmp/emails/'
 if not os.path.exists(EMAIL_PATH):
@@ -42,14 +42,24 @@ def get_content(msg):
 
 def split_content(content, maxrow=36):
     if isinstance(content, str):
-        lines = content.replace('"',"'").replace('`',"'").split('\n')
+        lines = content.replace('"',"”").replace('`',"｀").replace("'",'’').split('\n')
     else:
         lines = content.decode("utf-8").split('\n')
-        print(lines)
 
     text, omitted = "", ""
     current = 0
     for i, line in enumerate(lines):
+        # line = ""
+        # prev = 0
+        # while prev > -1:
+        #     index = text.find('<', prev)
+        #     line += text[prev:index]
+        #     if index > -1:
+        #         pos = text.find('>', index)
+        #         prev = pos
+        #     else:
+        #         break
+
         if current >= maxrow:
             # text += '⋮║ The orininal mail has more %d line(s) omitted.'%(len(lines) - current)
             omitted = ' The orininal mail has more %d line(s) omitted.'%(len(lines) - current)
@@ -62,7 +72,7 @@ def split_content(content, maxrow=36):
                 pos -= 1
             if pos>0:
                 if current == 0:
-                    text += ' ' + line[0:pos]+'\n'
+                    text += ' ' + line[0:pos]+'\n'
                 else:
                     text += '  ' + line[0:pos]+'\n'
                 current += 1
@@ -70,7 +80,7 @@ def split_content(content, maxrow=36):
             text += '\n'
             current += 1
     filename = EMAIL_PATH + ADDRESS[user] + '/content_%d'%note_id
-    print(filename)
+    # print(filename)
     os.system('echo "%s\n%s" > %s'%(text, omitted, filename))
 
 for user, _ in enumerate(ADDRESS):
@@ -78,14 +88,13 @@ for user, _ in enumerate(ADDRESS):
     imapobj = imaplib.IMAP4_SSL(SERVER[user], '993')
     imapobj.login(ADDRESS[user], PASSWORD[user])
 
-    #workラベル下のメールを取得
     imapobj.select()
-    typ, data = imapobj.search(None, 'All')
+    typ, data = imapobj.search(None, 'ALL')
+    NEW_NUM=len(data[0])
 
-    #取得したメールから表題と本文を出力し、添付ファイルを同階層に書き出す
     select = data[0].split()
     SHOW_NUM = 5
-    for note_id in range(SHOW_NUM+1):
+    for note_id in range(min(NEW_NUM, SHOW_NUM+1)):
         num = select[-note_id-1]
         typ, data = imapobj.fetch(num, '(RFC822)')
         mail = email.message_from_string(data[0][1].decode('utf-8'))
@@ -94,6 +103,10 @@ for user, _ in enumerate(ADDRESS):
 
         date = str(make_header(decode_header(mail["Date"])))
         pos = date.find('+')
+        if pos < 0:
+            pos = date.find('-')
+        if pos < 0:
+            pos = date.find('GMT')
         day = date[0: pos - 10]
         hms = date[pos - 9: pos - 1]
 
@@ -114,7 +127,7 @@ for user, _ in enumerate(ADDRESS):
             output += '   %d Attachment(s)\n%s'%(count, attachments)
 
         filename = EMAIL_PATH + ADDRESS[user] + '/header_%d'%note_id
-        print(filename)
+        # print(filename)
         os.system('echo "%s" > %s'%(output, filename))
 
         content = get_content(mail)
