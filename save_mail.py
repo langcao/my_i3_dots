e1afed0ab118769cde64521b75d82cd39355711c
Lang Cao <cao@ircn.jp>
2021-03-05 17:52:28 +0900

    Dot scripts updated (i3&py3status).

--- save_mail.py
+++ save_mail.py
@@ -2,13 +2,16 @@ import imaplib
 import email
 import base64
 import quopri
-import os, sys, time
+import codecs
+import os, sys, re, datetime
 from email.header import decode_header, make_header
 
 NAME = ['Lang Cao', 'Xun Li', 'Lang Cao', 'Xun Li']
 ADDRESS = ['lcao@zzu.edu.cn', 'li_xun@ircn.jp', 'cao@ircn.jp', 'milkquick@gmail.com']
 PASSWORD = ['miaomiao1982', '20200608lixun', 'miaomiao1982','kkmiudnpygdsyflj']
 SERVER = ['mail.v.zzu.edu.cn', 'ircn.sakura.ne.jp', 'ircn.sakura.ne.jp', 'imap.gmail.com']
+
+SHOW_NUM = 10
 
 EMAIL_PATH = '/tmp/emails/'
 if not os.path.exists(EMAIL_PATH):
@@ -42,24 +45,19 @@ def get_content(msg):
 
 def split_content(content, maxrow=36):
     if isinstance(content, str):
-        lines = content.replace('"',"”").replace('`',"｀").replace("'",'’').split('\n')
+        content = re.sub(r'<.*?>', '', content)
+        lines = content.split('\n')
     else:
-        lines = content.decode("utf-8").split('\n')
+        temp = codecs.decode(content)
+        lines = temp.split('\n')
 
-    text, omitted = "", ""
+    filename = EMAIL_PATH + ADDRESS[user] + '/content_%d'%note_id
+    f = open(filename, 'w')
+
     current = 0
+    omitted = ""
     for i, line in enumerate(lines):
-        # line = ""
-        # prev = 0
-        # while prev > -1:
-        #     index = text.find('<', prev)
-        #     line += text[prev:index]
-        #     if index > -1:
-        #         pos = text.find('>', index)
-        #         prev = pos
-        #     else:
-        #         break
-
+        text = ""
         if current >= maxrow:
             # text += '⋮║ The orininal mail has more %d line(s) omitted.'%(len(lines) - current)
             omitted = ' The orininal mail has more %d line(s) omitted.'%(len(lines) - current)
@@ -72,16 +70,22 @@ def split_content(content, maxrow=36):
                 pos -= 1
             if pos>0:
                 if current == 0:
-                    text += ' ' + line[0:pos]+'\n'
+                    text = ' ' + line[0:pos]+'\n'
                 else:
-                    text += '  ' + line[0:pos]+'\n'
+                    text = '  ' + line[0:pos]+'\n'
                 current += 1
         else:
-            text += '\n'
+            text = '\n'
             current += 1
-    filename = EMAIL_PATH + ADDRESS[user] + '/content_%d'%note_id
-    # print(filename)
-    os.system('echo "%s\n%s" > %s'%(text, omitted, filename))
+
+        f.write(text.replace('"',"”").replace("'",'’'))
+
+    f.write(omitted)
+    f.close()
+
+now = datetime.datetime.now()
+moment = str(now.time())
+print(moment[0:moment.find('.')-3])
 
 for user, _ in enumerate(ADDRESS):
 
@@ -93,7 +97,6 @@ for user, _ in enumerate(ADDRESS):
     NEW_NUM=len(data[0])
 
     select = data[0].split()
-    SHOW_NUM = 3
     for note_id in range(min(NEW_NUM, SHOW_NUM)):
         output = " %s <%s> (%d)\n"%(NAME[user], ADDRESS[user], note_id+1)
 
@@ -113,7 +116,10 @@ for user, _ in enumerate(ADDRESS):
         hms = date[pos - 9: pos - 1]
 
         sender = str(make_header(decode_header(mail["From"])))
-        receiver = str(make_header(decode_header(mail["To"])))
+        try:
+            receiver = str(make_header(decode_header(mail["To"])))
+        except:
+            receiver =  "Unknown"
         output += "\n %s  %s\n %s   %s"%(sender, receiver, day, hms)
 
         count, attachments = 0, ""
@@ -130,7 +136,7 @@ for user, _ in enumerate(ADDRESS):
             output += '   %d Attachment(s)\n%s'%(count, attachments)
 
         filename = EMAIL_PATH + ADDRESS[user] + '/header_%d'%note_id
-        print(filename)
+        output = output.replace('"','”')
         os.system('echo "%s" > %s'%(output, filename))
 
         content = get_content(mail)
